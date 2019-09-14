fb375e1dd42606ab1b795a273dc10776aaf34fe1
Yash Sharan <yash.sharan@oyorooms.com>
2019-09-14 16:53:12 +0530

    initial commit

---
+++ precache-manifest.4484c245ce9024ea2f652cd27cfc84f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "09828e6c4c00b138a10c",
+    "url": "/React-Portfolio/static/css/main.4efbcf99.chunk.css"
+  },
+  {
+    "revision": "09828e6c4c00b138a10c",
+    "url": "/React-Portfolio/static/js/main.09828e6c.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/React-Portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "804d233a10add83efa8d",
+    "url": "/React-Portfolio/static/js/runtime~main.804d233a.js"
+  },
+  {
+    "revision": "acf7fe5587f55b11a0455368079a7825",
+    "url": "/React-Portfolio/index.html"
+  }
+];
